677ade8a68fa64accc0b4bfacb8ac1260f77a1ed
peninah-njeri <peninahgathuru@gmail.com>
2019-09-30 09:15:14 +0300

    add environment folder to gitignore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,16 @@
-import { User } from './classes/user';
-import { GithubApiService } from './services/github-api.service';
-import { Component, OnInit } from '@angular/core';
-import { Repo } from './classes/repo';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent  {
 
   constructor(){
 
   }
-  ngOnInit(){
+ 
 
-  }
-
-
-  
 }
